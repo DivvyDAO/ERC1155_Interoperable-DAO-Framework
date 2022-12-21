70d059758633e7e721451aa85ed99e6f8fca887e
Jeffrey Anthony <jeffanthonyfds@gmail.com>
2022-12-21 08:10:22 -0500

    initial Contract attempt

--- contracts/Dyvvy.sol
+++ contracts/Dyvvy.sol
@@ -1,7 +1,27 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.6.5 <0.9.0;
 
-contract Dyvvy {
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+import "./ManagementControl.sol";
+
+contract Dyvvy is ERC1155, ManagementControl {
   constructor() public {
+
   }
+
+  uint128 daoCount;
+
+  function createDao(string memory daoName, uint256 initialMint) public payable {
+    _mint
+  }
+
+  function mint(uint256 id, address account, uint256 amount, bytes memory data)
+        public
+        onlyRole(TECH_EXEC)
+    {
+        require(id < 128), "Id out of range";
+        _mint(account, id, amount, data);
+    }
 }
--- contracts/ManagementControl.sol
+++ contracts/ManagementControl.sol
@@ -1,7 +1,26 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.6.5 <0.9.0;
 
-contract ManagementControl {
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+
+enum Role {
+  LEAD_LINK,
+  TECH_EXEC,
+  GOVERNANCE_CIRCLE,
+  SECRETARY,
+  DEPUTY
+}
+contract ManagementControl is AccessControlEnumerable {
+    bytes32 public constant LEAD_LINK = keccak256("LEAD_LINK");
+    bytes32 public constant TECH_EXEC = keccak256("TECH_EXEC");
+    bytes32 public constant GOVERNANCE_CIRCLE = keccak256("GOVERNANCE_CIRCLE");
+    bytes32 public constant SECRETARY = keccak256("SECRETARY");
+    bytes32 public constant DEPUTY = keccak256("DEPUTY");
   constructor() public {
+    _grantRole(LEAD_LINK, msg.sender);
+    _grantRole(TECH_EXEC, msg.sender);
+    _grantRole(GOVERNANCE_CIRCLE, msg.sender);
+    _grantRole(SECRETARY, msg.sender);
+    _grantRole(DEPUTY, msg.sender);
   }
 }
