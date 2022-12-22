31f410bf2b823b8fb55b935aa26b41647b88ed10
Jeffrey Anthony <jeffanthonyfds@gmail.com>
2022-12-22 06:44:07 -0500

    added README

---
+++ README.md
@@ -0,0 +1,6 @@
+## Dyvvy Dao ##
+
+Requirements
+[x] Adheres to the ERC1155 Standard
+[-] ManagedAccounts allow 2 of 5 minting, or just the TECH_EXEC
+[-] 
--- contracts/Dyvvy.sol
+++ contracts/Dyvvy.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.6.5 <0.9.0;
+pragma solidity >=0.7.4 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
@@ -8,15 +8,19 @@ import "./ManagementControl.sol";
 
 contract Dyvvy is ManagementControl, ERC1155Burnable, ERC1155Supply, ERC1155URIStorage {
   
-  constructor() ERC1155("Dyvvy") {
+  uint128 _daoCount;
+  mapping(string => uint256) private _daoNametoId;
+  mapping(uint256 => string) private _tokenURIs;
+  mapping(uint256 => uint256) private _totalSupply;
+
+  constructor() ERC1155("Dyvvy.com") {
     _daoCount = 0;
-    _mint(msg.sender, _daoCount*2, 100000, "");
+    _mint(msg.sender, _daoCount, 100000, "");
+    _daoCount += 7;
   }
 
   // Obligatory Overrides
-
-  mapping(uint256 => string) private _tokenURIs;
-  mapping(uint256 => uint256) private _totalSupply;
+ 
   function _beforeTokenTransfer(
         address operator,
         address from,
@@ -52,28 +56,43 @@ contract Dyvvy is ManagementControl, ERC1155Burnable, ERC1155Supply, ERC1155URIS
             interfaceId == type(IERC1155MetadataURI).interfaceId ||
             super.supportsInterface(interfaceId);
     }
+
     function uri(uint256 tokenId) public view virtual override(ERC1155, ERC1155URIStorage) returns (string memory) {
-        string memory tokenURI = _tokenURIs[tokenId];
+        string memory URI = _tokenURIs[tokenId];
 
         // If token URI is set, concatenate base URI and tokenURI (via abi.encodePacked).
-        return bytes(tokenURI).length > 0 ? string(abi.encodePacked(tokenURI)) : super.uri(tokenId);
+        return bytes(URI).length > 0 ? string(abi.encodePacked(URI)) : super.uri(tokenId);
     }
 
 
 
-  uint128 _daoCount;
-  mapping(string => uint256) private _daoNametoId;
 
   function createDao(string memory daoName, uint256 initialMint) public payable {
     _mint(msg.sender, _daoCount*2, initialMint, "");
+    _daoNametoId[daoName] = _daoCount;
     _daoCount++;
   }
 
-  function mint(uint256 id, address account, uint256 amount, bytes memory data)
+  function _getIdbyName(string memory daoName) public view returns(uint256){
+    return _daoNametoId[daoName];
+
+  }
+
+  function mintDAOtokens(uint256 id, address account, uint256 amount, bytes memory data)
     public
     onlyRole(TECH_EXEC)
   {
-    require(id < 128); "Id out of range";
+    if(id < 128) revert("Id out of range");
     _mint(account, id, amount, data);
   }
+
+  function mintDAOManagerTokens(uint256 id, address account, uint256 amount, bytes memory data)
+    public
+    onlyRole(TECH_EXEC)
+  {
+    if(id < 128) revert("Id out of range");
+    _mint(account, id, amount, data);
+  }
+
+  
 }
