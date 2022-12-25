0ff0b3d293e7251c24fba667511966afa2978d54
Jeffrey Anthony <jeffanthonyfds@gmail.com>
2022-12-25 07:27:22 -0500

    uri appears to be working, interface incomplete

--- contracts/TrustlessDAO.sol
+++ contracts/TrustlessDAO.sol
@@ -6,76 +6,56 @@ import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 import "./ManagementControl.sol";
 
-contract Trustless is ManagementControl, ERC1155Burnable, ERC1155Supply, ERC1155URIStorage {
+contract DAO is ManagementControl, ERC1155Burnable, ERC1155Supply, ERC1155URIStorage {
   
-  uint128 _daoCount;
-  mapping(string => uint256) private _daoNametoId;
-  mapping(uint256 => string) private _tokenURIs;
-  mapping(uint256 => uint256) private _totalSupply;
+  uint128 public _daoCount = 0;
+  mapping(string => uint256) public _daoNametoId;
+  mapping(uint256 => string) public _tokenURIs;
+  mapping(uint256 => uint256) public _totalSupply;
 
-  constructor() ERC1155("TrustlessTokens.com") {
-    _daoCount = 0;
-    _mint(msg.sender, _daoCount, 100000, "");
-    _daoCount += 7;
+  constructor() ERC1155("TrustlessDAO.net") {
+    createDao("Trustless DAO", 100000, "TrustlessDAO.net/metadata");
+    createDaowithExtradata("Twofold", 42, "http://127.0.0.1/nft/{-}.json", "0x34");
+    createDao("Third Heaven", 144000, "");
+    
   }
 
   // Obligatory Overrides
  
   function _beforeTokenTransfer(
-        address operator,
-        address from,
-        address to,
-        uint256[] memory ids,
-        uint256[] memory amounts,
-        bytes memory data
-    ) internal virtual override(ERC1155, ERC1155Supply) {
-        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
-
-        if (from == address(0)) {
-            for (uint256 i = 0; i < ids.length; ++i) {
-                _totalSupply[ids[i]] += amounts[i];
-            }
-        }
-
-        if (to == address(0)) {
-            for (uint256 i = 0; i < ids.length; ++i) {
-                uint256 id = ids[i];
-                uint256 amount = amounts[i];
-                uint256 supply = _totalSupply[id];
-                require(supply >= amount, "ERC1155: burn amount exceeds totalSupply");
-                unchecked {
-                    _totalSupply[id] = supply - amount;
-                }
-            }
-        }
+        address,
+        address,
+        address,
+        uint256[] memory,
+        uint256[] memory,
+        bytes memory
+    ) override(ERC1155, ERC1155Supply) internal virtual view  {
+        ERC1155Supply._beforeTokenTransfer;
     }
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override(AccessControlEnumerable, ERC1155) returns (bool) {
-        return
-            interfaceId == type(IERC1155).interfaceId ||
-            interfaceId == type(IERC1155MetadataURI).interfaceId ||
-            super.supportsInterface(interfaceId);
+  function supportsInterface(bytes4 interfaceId) public view virtual override(AccessControlEnumerable, ERC1155) returns (bool) {
+        return ERC1155.supportsInterface(interfaceId);
     }
 
-    function uri(uint256 tokenId) public view virtual override(ERC1155, ERC1155URIStorage) returns (string memory) {
-        string memory URI = _tokenURIs[tokenId];
-
-        // If token URI is set, concatenate base URI and tokenURI (via abi.encodePacked).
-        return bytes(URI).length > 0 ? string(abi.encodePacked(URI)) : super.uri(tokenId);
+  function uri(uint256 tokenId) public view virtual override(ERC1155, ERC1155URIStorage) returns (string memory) {
+        return ERC1155URIStorage.uri(tokenId);
     }
 
-
-
-
-  function createDao(string memory daoName, uint256 initialMint) public payable {
-    _mint(msg.sender, _daoCount*2, initialMint, "");
+  function createDao(string memory daoName, uint256 initialSupplyMint, string memory _uri) public returns (uint256 daoCount) {
+    _mint(msg.sender, _daoCount*2, initialSupplyMint, "0x0");
     _daoNametoId[daoName] = _daoCount;
+    _tokenURIs[_daoCount*2] = _uri;
     _daoCount++;
+    return daoCount;
   }
 
-  function _getIdbyName(string memory daoName) public view returns(uint256){
-    return _daoNametoId[daoName];
 
+  function createDaowithExtradata(string memory daoName, uint256 initialSupplyMint, string memory _uri, bytes memory data) public returns (uint256 daoCount) {
+    _mint(msg.sender, _daoCount*2, initialSupplyMint, data);
+    _daoNametoId[daoName] = _daoCount;
+    _tokenURIs[_daoCount*2] = _uri;
+    _daoCount++;
+    return daoCount;
   }
 
   function mintDAOtokens(uint256 id, address account, uint256 amount, bytes memory data)
@@ -93,6 +73,4 @@ contract Trustless is ManagementControl, ERC1155Burnable, ERC1155Supply, ERC1155
     if(id < 128) revert("Id out of range");
     _mint(account, id, amount, data);
   }
-
-  
 }
