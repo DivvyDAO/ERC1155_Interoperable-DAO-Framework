2763fdba0e92d87a773043a0f3db7ca5590483ae
Jeffrey Anthony <jeffanthonyfds@gmail.com>
2022-12-28 12:20:14 -0500

    Update DAONation.sol
    Signed-off-by: Jeffrey Anthony <jeffanthonyfds@gmail.com>

--- contracts/DAONation.sol
+++ contracts/DAONation.sol
@@ -4,17 +4,15 @@ pragma solidity >=0.7.4 <0.9.0;
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 import "./DAOManagement.sol";
 
-contract Trustless_DAO is DAOManagers, ERC1155Burnable {
+contract DAONation is DAOManagers, ERC1155Burnable {
   
   uint128 public _daoCount = 0;
-  mapping(string => uint256) public _daoNametoId;
+  mapping(string => uint256) private _daoNametoId;
   mapping(uint256 => string) public _tokenURIs;
   mapping(uint256 => uint256) private _totalSupply;
 
-    constructor() ERC1155("TrustlessDAO.net") {
-      createDao("Trustless DAO", 100000, "TrustlessDAO.net/metadata");
-      createDaowithExtradata("Twofold", 42, "http://127.0.0.1/nft/{-}.json", "0x34");
-      createDao("Third Heaven", 144000, "0x00");
+    constructor() ERC1155("DAONation.com") {
+      createDao("DAO Nation", 100000, "http://DAONation.com/metadata/DAONation.json");
       
     }
 
@@ -26,7 +24,6 @@ contract Trustless_DAO is DAOManagers, ERC1155Burnable {
 
     function uri(uint256 tokenId) public view virtual override(ERC1155) returns (string memory) {
       return _tokenURIs[tokenId];
-      // if calling managerId fall back to dao uri, management details will be found in that metadata
     }
 
     /**
@@ -89,18 +86,15 @@ contract Trustless_DAO is DAOManagers, ERC1155Burnable {
     public
     onlyManager(id)
   {
-    if(id > 2^128) revert("Id out of range, these are Manager Tokens");
     super._mint(account, id, amount, data);
   }
 
-  function mintDAOManagerTokens(uint256 id, address account, uint256 amount, bytes memory data) //thinking about removing the managertokens and making a simpleManagerVote without them
+  function mintDAOtokens(string memory id, address account, uint256 amount, bytes memory data)
     public
-    onlyManager(id)
+    onlyManager(_daoNametoId[id])
   {
-    if(id < 2^128) revert("Id out of range, these are Member Tokens");
-    super._mint(account, id, amount, data);
+    super._mint(account, _daoNametoId[id], amount, data);
   }
-
 
   /**
     * @dev Total amount of tokens in with a given id.
