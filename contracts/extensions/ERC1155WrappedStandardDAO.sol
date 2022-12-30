574851bd900222a77ccbf235470aebfeceea93a7
Jeffrey Anthony <jeffanthonyfds@gmail.com>
2022-12-30 09:58:35 -0500

    createDAO is fixed, may have edge case at DAOId[0]

--- contracts/extensions/ERC1155WrappedStandardDAO.sol
+++ contracts/extensions/ERC1155WrappedStandardDAO.sol
@@ -1,10 +1,3 @@
 // SPDX-License-Identifier: Proprietary
 // Copyright 2022 Tech Enterprises, Trustless, Jeff Anthony
 
-pragma solidity ^0.8.17;
-
-import "https://github.com/blockchainsllc/DAO/blob/develop/DAO.sol";
-
-abstract contract TEST is DAO {
-
-}
